6d72df3bf0652c60ec4ee1f3fe6366fa10fb0e6d
Julia Vamboldt <26203359+julvmdt@users.noreply.github.com>
2021-11-04 11:12:07 +0100

    add toggle button and some styling adjustments

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import ResultCard from "./ResultCard";
+import "./App.scss";
 
 const App = () => {
   return (
     <>
-      <h1>devfinder</h1>
+      <div className="app-title-container">
+        <h1>devfinder</h1>
+        <div className="app-toggle-container">
+          <h4 className="h4-bold">DARK</h4>
+          <img src={`${process.env.PUBLIC_URL}/icon-moon.svg`} />
+        </div>
+      </div>
       <SearchBar />
-      {/* <ResultCard /> */}
+      <ResultCard />
     </>
   );
 };
